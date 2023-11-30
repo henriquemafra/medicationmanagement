86963c0c156b91bfd58a7b1c4708090b910990d4
Henrique Mafra <henriquemafra89@gmail.com>
2023-11-30 11:48:58 -0300

    First Commit

---
+++ src/main/java/com/medicationmanagement/medicationmanagement/MedicationmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.medicationmanagement.medicationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicationmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicationmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicationmanagement/medicationmanagement/MedicationmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicationmanagement.medicationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicationmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
